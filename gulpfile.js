a7c4678a43933a258027d89c72b35b150bfba056
zhenshuai <zhenshuai@worktile.com>
2016-06-14 18:12:24 +0800

    2333

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var less = require('gulp-less');
+var path = require('path');
 
-//region Less
-
-gulp.task('lessc', function () {
-    return gulp.src('www/static/shared/css/shared')
-        .pipe(less())
-        .pipe('www/static/dist/all.css');
+gulp.task('less', function () {
+    return gulp.src('www/static/shared/css/*.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('www/static/dist/css'));
 });
 
-//endregion
-
-gulp.task('default', ['lessc']);
+gulp.task('default', ['less']);
